cb34f1dd1c61b9c89cd494e86677d5945d03959f
Jeremy.W <taurusjun@hotmail.com>
2024-09-07 12:19:25 +0800

    save

--- src/config/menuItems.js
+++ src/config/menuItems.js
@@ -2,7 +2,7 @@ export const menuItems = [
   { text: "欢迎", link: "/" },
   { text: "我的考试", link: "/my-exam/list" },
   { text: "错题集", link: "/error-questions" }, // 更新了文本和链接
-  { text: "考卷中心", link: "/exam/list" },
+  { text: "出卷", link: "/exam/list" },
   { text: "题库", link: "/questions" },
   { text: "用户中心", link: "/user-center" },
 ];
