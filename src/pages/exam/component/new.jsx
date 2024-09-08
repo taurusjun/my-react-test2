2a95a089fb22a56054f7539fac8973f232d6d279
Jeremy.Wang <taurusjun@hotmail.com>
2024-09-08 17:59:51 +0800

    save

--- src/pages/exam/component/new.jsx
+++ src/pages/exam/component/new.jsx
@@ -7,20 +7,27 @@ import {
   Box,
   FormControl,
   InputLabel,
+  Grid,
+  Typography,
+  Paper,
 } from "@mui/material";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
+import { useDictionaries } from "../../../provider/hooks/useDictionaries";
+import MultiLevelSelect from "../../../provider/components/MultiLevelSelect";
 
 const NewExam = ({ onExamCreated }) => {
-  const { control, handleSubmit } = useForm();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { dictionaries } = useDictionaries();
 
   const onSubmit = async (data) => {
     try {
-      // 发送创建考试的请求
       const response = await axios.post("/api/exam/create", data);
       const newExamUuid = response.data.uuid;
-
-      // 调用父组件传入的回调函数，传递新创建的考试UUID
       onExamCreated(newExamUuid);
     } catch (error) {
       console.error("创建考试失败:", error);
@@ -32,56 +39,79 @@ const NewExam = ({ onExamCreated }) => {
     <Box
       component="form"
       onSubmit={handleSubmit(onSubmit)}
-      sx={{ maxWidth: 400, mt: 2 }}
+      sx={{ maxWidth: 600, mt: 2 }}
     >
-      <Controller
-        name="name"
-        control={control}
-        defaultValue=""
-        rules={{ required: "请输入考试名称" }}
-        render={({ field, fieldState: { error } }) => (
-          <TextField
-            {...field}
-            label="名称"
-            fullWidth
-            margin="normal"
-            error={!!error}
-            helperText={error?.message}
-          />
-        )}
-      />
-      <Controller
-        name="category"
-        control={control}
-        defaultValue=""
-        rules={{ required: "请选择科目" }}
-        render={({ field, fieldState: { error } }) => (
-          <FormControl fullWidth margin="normal" error={!!error}>
-            <InputLabel>科目</InputLabel>
-            <Select {...field} label="科目">
-              <MenuItem value="math">数学</MenuItem>
-              <MenuItem value="english">英语</MenuItem>
-              {/* 添加更多科目选项 */}
-            </Select>
-          </FormControl>
-        )}
-      />
-      <Controller
-        name="stage"
-        control={control}
-        defaultValue=""
-        rules={{ required: "请选择阶段" }}
-        render={({ field, fieldState: { error } }) => (
-          <FormControl fullWidth margin="normal" error={!!error}>
-            <InputLabel>阶段</InputLabel>
-            <Select {...field} label="阶段">
-              <MenuItem value="primary">小学</MenuItem>
-              <MenuItem value="middle">初中</MenuItem>
-              <MenuItem value="high">高中</MenuItem>
-            </Select>
-          </FormControl>
-        )}
-      />
+      <Paper elevation={2} sx={{ p: 3, mb: 3, borderRadius: 2 }}>
+        <Typography variant="h6" gutterBottom sx={{ mb: 2 }}>
+          创建新考试
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Controller
+              name="name"
+              control={control}
+              defaultValue=""
+              rules={{ required: "请输入考试名称" }}
+              render={({ field, fieldState: { error } }) => (
+                <TextField
+                  {...field}
+                  label="考试名称"
+                  fullWidth
+                  required
+                  error={!!error}
+                  helperText={error?.message}
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Controller
+              name="category"
+              control={control}
+              defaultValue=""
+              rules={{ required: "请选择科目" }}
+              render={({ field, fieldState: { error } }) => (
+                <FormControl fullWidth required error={!!error}>
+                  <InputLabel id="category-select-label">科目</InputLabel>
+                  <Select
+                    {...field}
+                    labelId="category-select-label"
+                    label="科目"
+                  >
+                    {Object.entries(dictionaries.CategoryDict).map(
+                      ([key, value]) => (
+                        <MenuItem key={key} value={key}>
+                          {value}
+                        </MenuItem>
+                      )
+                    )}
+                  </Select>
+                </FormControl>
+              )}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Controller
+              name="gradeInfo"
+              control={control}
+              defaultValue={{ school: "", grade: "" }}
+              rules={{ required: "请选择学习阶段" }}
+              render={({ field, fieldState: { error } }) => (
+                <MultiLevelSelect
+                  onMultiSelectChange={(schoolLevel, grade) => {
+                    field.onChange({ school: schoolLevel, grade });
+                  }}
+                  initialSchoolLevel={field.value.school}
+                  initialGrade={field.value.grade}
+                  error={!!error}
+                  inline={true}
+                  fullWidth
+                />
+              )}
+            />
+          </Grid>
+        </Grid>
+      </Paper>
       <Button type="submit" variant="contained" sx={{ mt: 2 }}>
         创建考试
       </Button>
